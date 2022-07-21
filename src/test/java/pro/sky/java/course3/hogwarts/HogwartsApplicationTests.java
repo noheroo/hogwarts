93bfbd5cad2ee88358edf122aa8e5fdc6203292c
nohero_ <serega1552@yandex.ru>
2022-07-21 15:46:48 +0400

    Initial commit

---
+++ src/test/java/pro/sky/java/course3/hogwarts/HogwartsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course3.hogwarts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
